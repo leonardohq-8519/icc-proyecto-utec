706dbb6e8a9fe4346ddbe26ae643bf27d33c7000
Leonardo Huanay <leonardojulianhq@gmail.com>
2025-05-30 22:11:37 -0500

    Modified file_to_python in order to return types in an additional function. Added comments to the new function.

--- file_to_json.py
+++ file_to_json.py
@@ -32,3 +32,25 @@ def convert(repo:str, file_path:str):
     dex_json = json.loads(dex)
 
     return dex_json
+
+def retornar_tipos(nombres,tipos_json):
+    lista_tipos = []
+    lista_tipos_sec = []
+
+    #Recorre los nombres de todos los Pokemon
+    for nombre in nombres:
+        #Limpia el nombre de caracteres extraños
+        name = re.sub(r"\W+","",nombre)
+        #Brute Force a meowsticm (Está como meowstic en el otro archivo)
+        if name.lower() == "meowsticm":
+            name = name[:-1]
+        #Revisa el tipo primario según el .json y lo añade a la lista correspondiente
+        lista_tipos.append(tipos_json[name.lower()]["types"][0])
+        #De existir un tipo secundario en la lista del .json, lo añade a la lista de tipos secundarios
+        #De lo contrario se pone "None"
+        if len(tipos_json[name.lower()]["types"]) == 2:
+            lista_tipos_sec.append(tipos_json[name.lower()]["types"][1])
+        else:
+            lista_tipos_sec.append("None")
+    #Retorna ambas listas
+    return lista_tipos, lista_tipos_sec
